28fe3308c5b45134f4ede5e0bbf53db9f5ccf61f
Loaye <jr.carstens00@gmail.com>
2017-05-28 15:56:41 -0700

    Arrays of abilities, languages, and alignment

---
+++ ability.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Alignment possibilities
+alignmentArr = ['Lawful Good', 'Lawful Neutral', 'Lawful Evil', 'Neutral Good', 'True Neutral', 'Neutral Evil', 'Chaotic Good', 'Chaotic Neutral', 'Chaotic Evil'];
+
+//Languages
+languageArr = ['Abyssal', 'Aquan', 'Auran', 'Celestial', 'Common', 'Deep Speach', 'Draconic', 'Druidic', 'Dwarvish', 'Elvish', 'Giant', 'Gnomish', 'Goblin', 'Gnoll', 'Halfling', 'Ignan', 'Infernal', 'Orc', 'Primoridial', 'Sylvan', 'Terran', 'Undercommon'];
+
+//Ability Per Race/Class
+abilityHuman = [];
+
+abilityHalfling = ['Lucky', 'Brave', 'Halfling Nimbleness', 'Lightfoot'];
+
+abilityHalfOrc = ['Darkvision', 'Menacing', 'Relentless Endurance', 'Savage Attacks'];
+
+abilityElf = ['Darkvision', 'Keen Senses', 'Fey Ancestry', 'Trance'];
+
+abilityDragonborn = ['Draconic Ancestry Black Acid', 'Draconic Ancestry Blue Lightning', 'Draconic Ancestry Brass Fire', 'Draconic Ancestry Bronze Lightning', 'Draconic Ancestry Copper Acid', 'Draconic Ancestry Gold Fire', 'Draconic Ancestry Green Poison', 'Draconic Ancestry Red Fire', 'Draconic Ancestry Silver Cold', 'Draconic Ancestry White Cold', 'Breath Weapon', 'Damage Resistance'];
+
+abilityDwarf = ['Darkvision', 'Dwarven Resilience', 'Dwarven Combat Training', 'Dwarven Toughness', 'Tool Profiency', 'Stonecunning'];
