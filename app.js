d86442ac0f55b6e48d7a94b1453305776010837f
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-05-29 11:10:13 -0700

    Instantiaed predifined classes and races

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 'use strict';
+
+var dropdown = document.getElementsByClassName('hidden master')[0];
+var menu = document.getElementsByClassName('menu')[0];
+
+function toggle() {
+  if(dropdown.style.display === 'none') {
+    dropdown.style.display = 'inline';
+  } else {
+    dropdown.style.display = 'none';
+  }
+}
+
+menu.addEventListener('click', toggle);
--- constructor.js
+++ constructor.js
@@ -1,11 +1,10 @@
 var allRaces = [];
 var allClasses = [];
 
-function CharClass(className, stats, description, abilities) {
+function CharClass(className, stats, description) {
   this.name = className;
   this.stats = stats;
   this.description = description;
-  this.abilities = abilities;
   allClasses.push(this);
 }
 
@@ -16,15 +15,20 @@ function Race(race, stats, description, abilities) {
   this.abilities = abilities;
   allRaces.push(this);
 }
+//Array order for stats: Charima, Constitution, Dexterity, Intelligence, Strength, Wisdom
+var dragonborn = new Race('DragonBorn', [1, 0, 0, 0, 2, 0], 'I don\'t know', [])
+var elf = new Race('Elf', [0, 2, 0, 0, 0, 1], 'I don\'t know', [])
+var dwarf = new Race('Dwarf', [0, 0, 2, 0, 0, 1], 'I don\'t know', [])
+var halfling = new Race('Halfling', [1, 0, 2, 0, 0, 0], 'I don\'t know', [])
+var halfOrc = new Race('DragonBorn', [0, 1, 0, 0, 2, 0], 'I don\'t know', [])
+var human = new Race('Human', [1, 1, 1, 1, 1, 1], 'I don\'t know', [])
 
-var dragonborn = new Race('DragonBorn', [], 'I don\'t know', [])
-var elf = new Race('Elf', [], 'I don\'t know', [])
-var dwarf = new Race('Dwarf', [], 'I don\'t know', [])
-var halfling = new Race('Halfling', [], 'I don\'t know', [])
-var halfOrc = new Race('DragonBorn', [], 'I don\'t know', [])
-var human = new Race('Human', [], 'I don\'t know', [])
-
-var 
+var warrior = new CharClass('warrior', [0, 11, 0, 0, 2, 0], 'some stuff');
+var cleric = new CharClass('cleric', [1, 8, 0, 0, 0, 2], 'some stuff');
+var ranger = new CharClass('ranger', [0, 10, 2, 0, 1, 0], 'some stuff');
+var rogue = new CharClass('rogue', [0, 9, 2, 0, 0, 0], 'some stuff');
+var bard = new CharClass('bard', [1, 8, 2, 0, 0, 0], 'some stuff');
+var mage = new CharClass('mage', [0, 6, 1, 2, 0, 0], 'some stuff');
 
 
 function Player(name, playerClass, playerRace) {
