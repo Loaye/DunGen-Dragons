862cbb7b65521f4ebaf91e2dd9e6d2e711e73768
Anthony Konschuh <silv.amee@gmail.com>
2017-05-30 16:42:11 -0700

    cleaned code

--- home/ran-gen.js
+++ home/ran-gen.js
@@ -4,24 +4,22 @@ var ranGen = document.getElementsByName('instant');
 var ranClass;
 var ranRace;
 var ranName;
-var ranLanguage = ['Common', ];
-
-var allLanguages = ['Abyssal', 'Aquan', 'Auran', 'Celestial', 'Deep Speach', 'Draconic', 'Druidic', 'Dwarvish', 'Elvish', 'Giant', 'Gnomish', 'Goblin', 'Gnoll', 'Halfling', 'Ignan', 'Infernal', 'Orc', 'Primoridial', 'Sylvan', 'Terran', 'Undercommon'];
 
 function generateRandomPlayer(){
   var classSelector = Math.floor(Math.random() * allClasses.length);
   var raceSelector = Math.floor(Math.random() * allRaces.length);
-  var languageSelector = Math.floor(Math.random() * allLanguages.length);
   ranClass = allClasses[classSelector];
   ranRace = allRaces[raceSelector];
-  ranLanguage.push(allLanguages[languageSelector]);
+  console.log(raceSelector);
+  console.log(classSelector);
+  console.log(ranRace);
+  console.log(ranClass);
   packageValues();
 }
 
 function packageValues(){
   sessionStorage.charRace = JSON.stringify(ranRace);
   sessionStorage.charClass = JSON.stringify(ranClass);
-  sessionStorage.charLanguages = JSON.stringify(ranLanguage);
 }
 
 ranGen.addEventListener('click', generateRandomPlayer());
