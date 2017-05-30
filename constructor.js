82b573121eaec1f6a1e9a449f1727f5dda2b7ef2
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-05-30 10:21:19 -0700

    Added random stat generator that includes race and class bonuses

--- constructor.js
+++ constructor.js
@@ -20,19 +20,20 @@ var dragonborn = new Race('DragonBorn', [1, 0, 0, 0, 2, 0], 'I don\'t know', [])
 var elf = new Race('Elf', [0, 2, 0, 0, 0, 1], 'I don\'t know', []);
 var dwarf = new Race('Dwarf', [0, 0, 2, 0, 0, 1], 'I don\'t know', []);
 var halfling = new Race('Halfling', [1, 0, 2, 0, 0, 0], 'I don\'t know', []);
-var halfOrc = new Race('DragonBorn', [0, 1, 0, 0, 2, 0], 'I don\'t know', []);
+var halfOrc = new Race('Half Orc', [0, 1, 0, 0, 2, 0], 'I don\'t know', []);
 var human = new Race('Human', [1, 1, 1, 1, 1, 1], 'I don\'t know', []);
 
-var warrior = new CharClass('warrior', [0, 11, 0, 0, 2, 0], 'some stuff');
-var cleric = new CharClass('cleric', [1, 8, 0, 0, 0, 2], 'some stuff');
-var ranger = new CharClass('ranger', [0, 10, 2, 0, 1, 0], 'some stuff');
-var rogue = new CharClass('rogue', [0, 9, 2, 0, 0, 0], 'some stuff');
-var bard = new CharClass('bard', [1, 8, 2, 0, 0, 0], 'some stuff');
-var mage = new CharClass('mage', [0, 6, 1, 2, 0, 0], 'some stuff');
+var warrior = new CharClass('Warrior', [0, 11, 0, 0, 2, 0], 'some stuff');
+var cleric = new CharClass('Cleric', [1, 8, 0, 0, 0, 2], 'some stuff');
+var ranger = new CharClass('Ranger', [0, 10, 2, 0, 1, 0], 'some stuff');
+var rogue = new CharClass('Rogue', [0, 9, 2, 0, 0, 0], 'some stuff');
+var bard = new CharClass('Bard', [1, 8, 2, 0, 0, 0], 'some stuff');
+var mage = new CharClass('Mage', [0, 6, 1, 2, 0, 0], 'some stuff');
 
 
-function Player(name, playerClass, playerRace) {
+function Player(name, playerClass, playerRace, stats) {
   this.name = name;
   this.playerClass = playerClass;
   this.playerRace = playerRace;
+  this.stats = stats;
 }
