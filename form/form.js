72d89185a2127a1c90bfc80a951e50d02170602d
Anthony Konschuh <silv.amee@gmail.com>
2017-05-30 17:26:31 -0700

    added random language generator

--- form/form.js
+++ form/form.js
@@ -2,6 +2,7 @@
 var charClass;
 var charRace;
 var charLanguages;
+var charTraits;
 var finalStats = [];
 
 function unPack() {
@@ -9,6 +10,7 @@ function unPack() {
     charClass = JSON.parse(sessionStorage.charClass);
     charRace = JSON.parse(sessionStorage.charRace);
     charLanguages = JSON.parse(sessionStorage.charLanguages);
+    charTraits = JSON.parse(sessionStorage.charTraits);
     restrictValues();
   }
 }
@@ -38,14 +40,27 @@ function insertStats() {
 }
 
 function insertLanguages(){
-  var langTags = document.getElementsByClassName('char-languages');
+  var langTags = document.getElementById('char-languages');
   var list = document.createElement('ul');
+  var listArr = [];
   for (var i = 0; i < charLanguages.length; i++){
-    list.innerHTML = '<li>' + charLanguages[i] + '</li>';
-    console.log(list);
+    listArr.push('<li>' + charLanguages[i] + '</li>');
   }
-  langTags.appendChild = list;
+  list.innerHTML = listArr.join('');
+  langTags.appendChild (list);
+}
+
+function insertTraits(){
+  var traitTags = document.getElementById('char-traits');
+  var list = document.createElement('ul');
+  var listArr = [];
+  for (var i = 0; i < charTraits.length; i++){
+    listArr.push('<li>' + charTraits[i] + '</li>');
+  }
+  list.innerHTML = listArr.join('');
+  traitTags.appendChild (list);
 }
 
 insertStats();
 insertLanguages();
+insertTraits();
--- home/ran-gen.js
+++ home/ran-gen.js
@@ -4,22 +4,54 @@ var ranGen = document.getElementsByName('instant');
 var ranClass;
 var ranRace;
 var ranName;
+var ranTraits = [];
+var ranLanguage = ['Common', ];
+
+var allLanguages = ['Abyssal', 'Aquan', 'Auran', 'Celestial', 'Deep Speach', 'Draconic', 'Druidic', 'Dwarvish', 'Elvish', 'Giant', 'Gnomish', 'Goblin', 'Gnoll', 'Halfling', 'Ignan', 'Infernal', 'Orc', 'Primoridial', 'Sylvan', 'Terran', 'Undercommon'];
 
 function generateRandomPlayer(){
   var classSelector = Math.floor(Math.random() * allClasses.length);
   var raceSelector = Math.floor(Math.random() * allRaces.length);
+  var languageSelector = Math.floor(Math.random() * allLanguages.length);
   ranClass = allClasses[classSelector];
   ranRace = allRaces[raceSelector];
-  console.log(raceSelector);
-  console.log(classSelector);
-  console.log(ranRace);
-  console.log(ranClass);
+  ranLanguage.push(allLanguages[languageSelector]);
+  generateRandomTraits();
   packageValues();
+}
+
+function generateRandomTraits(){
+  var traitNumber = Math.floor(Math.random()*3 + 1);
+  while(ranTraits.length < traitNumber){
+    var traitSelector = Math.floor(Math.random() * ranRace.traits.length);
+    return ranTraits.push(ranRace.traits[traitSelector]);
+  }
 }
 
 function packageValues(){
   sessionStorage.charRace = JSON.stringify(ranRace);
   sessionStorage.charClass = JSON.stringify(ranClass);
+  sessionStorage.charLanguages = JSON.stringify(ranLanguage);
+  sessionStorage.charTraits = JSON.stringify(ranTraits);
 }
 
 ranGen.addEventListener('click', generateRandomPlayer());
+
+
+// function generateRandomPlayer(){
+//   var classSelector = Math.floor(Math.random() * allClasses.length);
+//   var raceSelector = Math.floor(Math.random() * allRaces.length);
+//   var languageSelector = Math.floor(Math.random() * allLanguages.length);
+//   ranClass = allClasses[classSelector];
+//   ranRace = allRaces[raceSelector];
+//   ranLanguage.push(allLanguages[languageSelector]);
+//   packageValues();
+// }
+//
+// function packageValues(){
+//   sessionStorage.charRace = JSON.stringify(ranRace);
+//   sessionStorage.charClass = JSON.stringify(ranClass);
+//   sessionStorage.charLanguages = JSON.stringify(ranLanguage)
+// }
+//
+// ranGen.addEventListener('click', generateRandomPlayer());
