f09f16bb18ce4828ed5b082a983ddee4a23c5f15
Anthony Konschuh <silv.amee@gmail.com>
2017-05-31 14:53:23 -0700

    removed excess files, fixed background spacing

--- form/traits.js
+++
@@ -1,7 +0,0 @@
-
-// Alignment possibilities
-var alignmentArr = ['Lawful Good', 'Lawful Neutral', 'Lawful Evil', 'Neutral Good', 'True Neutral', 'Neutral Evil', 'Chaotic Good', 'Chaotic Neutral', 'Chaotic Evil'];
-
-//Languages
-
-var allLanguages = ['Abyssal', 'Aquan', 'Auran', 'Celestial', 'Deep Speach', 'Draconic', 'Druidic', 'Dwarvish', 'Elvish', 'Giant', 'Gnomish', 'Goblin', 'Gnoll', 'Halfling', 'Ignan', 'Infernal', 'Orc', 'Primoridial', 'Sylvan', 'Terran', 'Undercommon'];
